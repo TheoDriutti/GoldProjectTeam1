42bce4db89c1ad74be645a143475a00bac94bc67
Gastonx <56830127+Gastonx@users.noreply.github.com>
2020-05-06 10:46:17 +0200

    ProtoPosageDesObjets

---
+++ Assets/Hugo/Script/ItemManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    public List<GameObject> objectStored;
+    public List<GameObject> buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
