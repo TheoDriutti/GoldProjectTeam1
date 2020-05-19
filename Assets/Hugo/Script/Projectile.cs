6dbe3c82eb5aee6a0ea2dec6051f751780cd64e2
DAMIEN <damien.leclercq@edu.devinci.fr>
2020-05-19 14:48:07 +0200

    Salut, je m'apelle Damien Leclercq et voici mon premier push, j'espère que vous allez apprécier mes lvl
    Cordialement
    baslec
     torlrolrololololol

---
+++ Assets/Hugo/Script/Projectile.cs
@@ -0,0 +1,27 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed;
+    private ProjHolder endPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.MoveTowards(transform.position, endPoint.position, speed * Time.delataTime);
+
+    }
+}
+*/
